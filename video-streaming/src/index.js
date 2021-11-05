308537b7cf5c5aaf733f8e146bbf06443c1b887f
Mike Nightingale <michael.nightingale@candide.com>
2021-11-05 21:42:33 +0000

    Change video-streaming to use video-storage

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import fs from "fs";
+import http from "http";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const PATH = "videos/SampleVideo_1280x720_1mb.mp4";
+const { VIDEO_STORAGE_HOST, VIDEO_STORAGE_PORT } = process.env;
 
 app.get("*", (req, _res, next) => {
   console.log(`Request from ${req.ip} for route ${req.path}`);
@@ -15,15 +16,22 @@ app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/video", (_req, res) => {
-  const stats = fs.statSync(PATH);
+app.get("/video", (req, res) => {
+  const forwardReq = http.request(
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: "/video?path=SampleVideo_1280x720_1mb.mp4",
+      method: "GET",
+      headers: req.headers,
+    },
+    (forwardRes) => {
+      res.writeHead(forwardRes.statusCode, forwardRes.headers);
+      forwardRes.pipe(res);
+    }
+  );
 
-  res.writeHead(200, {
-    "Content-Length": stats.size,
-    "Content-Type": "video/mp4",
-  });
-
-  fs.createReadStream(PATH).pipe(res);
+  req.pipe(forwardReq);
 });
 
 app.listen(PORT, () => {
