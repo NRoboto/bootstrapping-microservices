4de435e2cbef70e3e29aeb3add2743aed45362b5
Mike Nightingale <michael.nightingale@candide.com>
2022-06-03 11:57:47 +0100

    Add Kubernetes provider for Terraform

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/google"
       version = "4.20.0"
     }
+
+    kubernetes ={
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
   }
 }
 
@@ -12,4 +17,19 @@ provider "google" {
 
   project = var.project
   region  = var.region
+}
+
+data "google_client_config" "default" {
+  depends_on = [
+    google_container_cluster.cluster
+  ]
+}
+
+provider "kubernetes" {
+  host = "https://${google_container_cluster.cluster.endpoint}"
+
+  token = data.google_client_config.default.access_token
+  client_certificate = base64decode(google_container_cluster.cluster.master_auth.0.client_certificate)
+  client_key = base64decode(google_container_cluster.cluster.master_auth.0.client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth.0.cluster_ca_certificate)
 }
